d42ee6bfad95533ec84bbd9f695e2a6a21fbab01
fatou099 <fatou.kone99@outlook.fr>
2023-02-15 17:32:15 +0100

    new project

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Example() {
+    return (
+        <div className="container mt-5">
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card text-center">
+                    <div className="card-header"><h2>Bienvenue chez Booking Flex Office</h2></div>
+                    <div className="card-body">Je suis une page de test</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById('example')) {
+    const Index = ReactDOM.createRoot(document.getElementById("example"));
+
+    Index.render(
+        <React.StrictMode>
+            <Example/>
+        </React.StrictMode>
+    )
+}
