f512d47352d0fe8028b8b11a18438beb9b23121d
fatou199 <65873642+fatou199@users.noreply.github.com>
2023-03-24 14:36:03 +0100

    commentaires dans register pour mieux comprendre les fonctions

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -14,6 +14,14 @@ export default function Register() {
         { label: "Chef de projet", value: "projet" },
 
     ]
+
+// data: qui représente l'état actuel des champs du formulaire.
+// setData: fonction pour mettre à jour l'état des champs du formulaire.
+// post: fonction pour envoyer une requête HTTP POST pour soumettre les données du formulaire.
+// processing: variable booléenne qui est true lorsque la soumission du formulaire est en cours.
+// errors: contient les erreurs de validation des champs du formulaire.
+// reset: fonction pour réinitialiser les valeurs des champs du formulaire à leurs valeurs initiales.
+
     const { data, setData, post, processing, errors, reset } = useForm({
         name: '',
         username: '',
@@ -24,16 +32,24 @@ export default function Register() {
 
     });
 
+
+    // mettre à jour la valeur de la fonction sélectionnée dans l'objet de données de l'application chaque fois que l'utilisateur sélectionne une nouvelle option dans une liste déroulante
+    // permet de stocker la fonction sélectionnée dans l'objet de données
     const onFonctionChange = (selectedOption) => {
         setData('fonction', selectedOption.label);
     };
 
+    //  ce code permet de nettoyer les valeurs de deux champs de mot de passe 
+    //  lorsqu'un composant est démonté ou que les dépendances du hook changent.
     useEffect(() => {
         return () => {
             reset('password', 'password_confirmation');
         };
     }, []);
 
+    // cette fonction est utilisée pour mettre à jour les données de l'application 
+    // en fonction de l'entrée de l'utilisateur, 
+    // en prenant en compte différents types d'entrées (valeur de saisie ou coche de case à cocher).
     const handleOnChange = (event) => {
         setData(event.target.name, event.target.type === 'checkbox' ? event.target.checked : event.target.value);
     };
