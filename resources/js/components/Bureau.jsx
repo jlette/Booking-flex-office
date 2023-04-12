34560f5d3c69a5428e090b9f4d71aac0ab5b604e
jlette <lettevoueto.jimmy@gmail.com>
2023-04-12 15:32:35 +0200

    Modification du composant bureau pour l'importer dans un layout.

--- resources/js/components/Bureau.jsx
+++ resources/js/components/Bureau.jsx
@@ -1,8 +1,9 @@
 import { MdEventSeat } from "react-icons/md";
 import { BsFillBuildingFill } from "react-icons/bs";
 import { useState } from "react";
+import "react-tooltip/dist/react-tooltip.css";
 import { Tooltip as ReactTooltip } from "react-tooltip";
-export default function Bureau(props) {
+export default function Bureau() {
     const [reserver, setReserver] = useState("");
     const [shadow, setShadow] = useState("");
     const handleOnChange = (event) => {
@@ -14,62 +15,15 @@ export default function Bureau(props) {
     };
 
     return (
-        <div className="w-50 h-50 bg-success ">
-            <div
-                className="container border border-dark flex justify-center"
-                style={{ padding: 25 }}
-            >
-                <div>
-                    <MdEventSeat
-                        onMouseEnter={handleOnMouse}
-                        onClick={handleOnChange}
-                        color={reserver}
-                        size={"2em"}
-                        className={{ shadow }}
-                    />
-
-                    <ReactTooltip type="succes" id="tool" />
-                    <MdEventSeat className="drop-shadow-2xl" size={"2em"} />
-                    <MdEventSeat size={"2em"} />
-                </div>
-                <div>
-                    <MdEventSeat size={"2em"} />
-                    <MdEventSeat size={"2em"} />
-                    <MdEventSeat size={"2em"} />
-                </div>
-                <div>
-                    <MdEventSeat size={"2em"} />
-                    <MdEventSeat size={"2em"} />
-                    <MdEventSeat size={"2em"} />
-                </div>
-                <div>
-                    <MdEventSeat size={"2em"} />
-                    <MdEventSeat size={"2em"} />
-                    <MdEventSeat size={"2em"} />
-                </div>
-                <div>
-                    <MdEventSeat size={"2em"} />
-                    <MdEventSeat size={"2em"} />
-                    <MdEventSeat size={"2em"} />
-                </div>
-            </div>
-
-            <h4 className="mt-3 flex justify-center">
-                <div className="flex row mr-3">
-                    <MdEventSeat color={"green"} size={"1.5em"} />
-                    <p className="">Mes places</p>
-                </div>
-
-                <div className="flex row  mr-3">
-                    <MdEventSeat size={"1.5em"} />
-                    <p className="">Places libre</p>
-                </div>
-
-                <div className="flex row  mr-3">
-                    <MdEventSeat color={"grey"} size={"1.5em"} />
-                    <p>Places occupé</p>
-                </div>
-            </h4>
+        <div data-tooltip-id="my-tooltip" data-tooltip-content="Place 42">
+            <ReactTooltip type="succes" id="my-tooltip" />
+            <MdEventSeat
+                onMouseEnter={handleOnMouse}
+                onClick={handleOnChange}
+                color={reserver}
+                size={"2em"}
+                className={{ shadow }}
+            />
         </div>
     );
 }
