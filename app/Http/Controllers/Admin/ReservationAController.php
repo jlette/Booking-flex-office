bee5bfefedc26d3a3e2ded47b2f70b7191c08b49
fatou199 <65873642+fatou199@users.noreply.github.com>
2023-05-05 21:01:03 +0200

    ajout des reservations

--- app/Http/Controllers/Admin/ReservationAController.php
+++ app/Http/Controllers/Admin/ReservationAController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\User;
 use Inertia\Inertia;
+use App\Models\Place;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class ReservationAController extends Controller
 {
     public function index(){
 
-        return Inertia::render('Admin/Reservations/ReservationIndex');
+        $reservation = DB::table('reservation')
+            ->select('*')
+            ->join('users', 'users.iduser', '=', 'reservation.id_user')
+            ->join('place', 'place.idplace', '=', 'reservation.id_place')
+            ->get();
+        
+        return Inertia::render('Admin/Reservations/ReservationIndex', [
+            'reservations' => $reservation,
+        ]);
+    
     }
+    
 }
