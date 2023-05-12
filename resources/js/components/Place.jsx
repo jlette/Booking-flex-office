5fa7716e50769030529b09e8f7fa9e3fca9c9e5e
fatou199 <65873642+fatou199@users.noreply.github.com>
2023-05-12 16:52:41 +0200

    alert place occupée

--- resources/js/components/Place.jsx
+++ resources/js/components/Place.jsx
@@ -23,9 +23,13 @@ export default function Place(props) {
     };
 
     const handleSelectPlace = () => {
+        if (colorPlace == 'grey') {
+            alert("La place est déjà occupée")
+        } else{
             onPlaceSelect(placeid);
             console.log(`Vous avez selectionner la place ${numplace} avec son ID ${placeid} et cette place ${colorPlace}`);
-        };
+        }
+    };
 
     return (
         <>
@@ -41,7 +45,7 @@ export default function Place(props) {
                 />
                 <label
                     for={placeid}
-                    class=" cursor-pointer peer-checked:border-green-600 peer-checked:text-green-600 hover:text-indigo-600 hover:bg-indigo-100 dark:text-indigo-400 dark:bg-indigo-800 dark:hover:bg-gray-indigo"
+                    class="cursor-pointer peer-checked:border-green-600 peer-checked:text-green-600 hover:text-indigo-500"
                 >
                     <div
                         key={placeid}
